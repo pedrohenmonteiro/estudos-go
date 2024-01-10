c3b645b85715f336fd00183b0812927dee21519b
Pedro Monteiro <pedromont436@gmail.com>
2024-01-09 22:09:27 -0300

    customizando a request

--- 26-HttpClient/2/main.go
+++ 26-HttpClient/2/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"bytes"
 	"io"
 	"net/http"
-	"os"
-	"time"
 )
 
 func main() {
-	c := http.Client{Timeout: time.Second}
-	jsonVar := bytes.NewBuffer([]byte(`{"name": "John", "age": 30}`))
-	resp, err := c.Post("http://google.com", "application/json", jsonVar)
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.CopyBuffer(os.Stdout, resp.Body, nil)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
 }
