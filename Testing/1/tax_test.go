071f7877898ef07e2994be8d9ae6e52a41b20eaf
Pedro Monteiro <pedromont436@gmail.com>
2024-01-11 15:22:34 -0300

    testando em batch

--- Testing/1/tax_test.go
+++ Testing/1/tax_test.go
@@ -12,3 +12,22 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("Expected %.1f but got %.1f", expected, result)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expected {
+			t.Errorf("Expected %.1f but got %.1f", item.expected, result)
+		}
+	}
+}
