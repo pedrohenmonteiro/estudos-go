8926e17434477be5b8feb1e33746c7e1b560fc45
Pedro Monteiro <pedromont436@gmail.com>
2024-01-11 16:16:41 -0300

    rodando testes com fuzzing

--- Testing/1/tax_test.go
+++ Testing/1/tax_test.go
@@ -44,6 +44,23 @@ func BenchmarkCalculateTax2(b *testing.B) {
 	}
 }
 
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 150.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Received %f but expected 20", result)
+		}
+	})
+}
+
 // go test -coverprofile=coverage.out  para ver a cobertura de testes
 
 // go tool cover -html=coverage.out  para vizualizar a cobertura de testes em html
@@ -53,3 +70,4 @@ func BenchmarkCalculateTax2(b *testing.B) {
 // go test -bench=. -run=^# -count=10 para rodar o benchmark 10 vezes
 // go test -bench=. -run=^# -count=10 -benchtime=3s roda o benchmark por 3 segundos
 // go test -bench=. -run=^# -benchmem   para verificar a alocaçao de memoria
+// go test -fuzz=. -run=^#     para testar com fuzzing
