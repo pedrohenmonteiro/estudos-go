cafa7bfdd5a886ddbc4d14c8115b07de47923294
Pedro Monteiro <pedromont436@gmail.com>
2024-01-11 01:37:48 -0300

    fazendo relacionamento hasMany com orm

---
+++ Banco-de-Dados/4/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3307)/cursogo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Product{}, &Category{})
+
+	db.Create(&Category{
+		Name: "Eletronicos",
+	})
+
+	db.Create(&Product{
+		Name:       "Mouse",
+		Price:      50.00,
+		CategoryID: 1,
+	})
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+
+	for _, category := range categories {
+		fmt.Println(category.Name, ":")
+		for _, product := range category.Products {
+			println("- ", product.Name)
+		}
+	}
+}
