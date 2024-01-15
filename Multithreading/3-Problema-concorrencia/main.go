ee029ad3334eaf044ea14d1e7cd049404bab1a75
Pedro Monteiro <pedromont436@gmail.com>
2024-01-15 20:59:25 -0300

    utilizando mutex para resolver problemas de concorrencia

--- Multithreading/3-Problema-concorrencia/main.go
+++ Multithreading/3-Problema-concorrencia/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
+	m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		m.Lock()
 		number++
+		m.Unlock()
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("Você é o visitante numero %d", number)))
 	})
@@ -19,3 +23,6 @@ func main() {
 
 //teste no apache benchmark
 //ab -n 10000 -c 100 http://localhost:3000/
+
+//verificar cenário de race condition:
+//go run -race main.go
