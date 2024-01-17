31a8f7e36c059a74b8adbe1afad31a1c7076c99c
Pedro Monteiro <pedromont436@gmail.com>
2024-01-17 00:13:21 -0300

    implementando e testando metodo Clear

--- Eventos/pkg/events/event_dispatcher_test.go
+++ Eventos/pkg/events/event_dispatcher_test.go
@@ -83,6 +83,26 @@ func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHand
 
 }
 
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Clear() {
+	//Event 1
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	//Event 2
+	err = suite.eventDispatcher.Register(suite.event2.GetName(), &suite.handler3)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event2.GetName()]))
+
+	suite.eventDispatcher.Clear()
+	suite.Equal(0, len(suite.eventDispatcher.handlers))
+
+}
+
 func TestSuite(t *testing.T) {
 	suite.Run(t, new(EventDispatcherTestSuite))
 }
