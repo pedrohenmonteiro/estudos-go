ebeb4736ca4ed815638c89cd75cc9c14891d3880
Pedro Monteiro <pedromont436@gmail.com>
2024-01-19 01:15:41 -0300

    implementando upload de arquivos com amazon s3 e go routines

---
+++ UploadS3/cmd/uploader/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+	wg       sync.WaitGroup
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"JHOFDIJSF942EXAMPLE",
+				"Owekjer09qj3eqkloawj",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "pedromonteiro-bucket-exemplo"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	uploadControl := make(chan struct{}, 100)
+	for {
+		files, err := dir.ReadDir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %s\n", err)
+			continue
+		}
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		go uploadFile(files[0].Name(), uploadControl)
+	}
+	wg.Wait()
+}
+
+func uploadFile(filename string, uploadControl <-chan struct{}) {
+	defer wg.Done()
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s", completeFileName, s3Bucket)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", completeFileName)
+		<-uploadControl // esvazia o canal
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s\n", completeFileName)
+		<-uploadControl // esvazia o canal
+		return
+	}
+	fmt.Printf("File %s uploaded successfully\n", completeFileName)
+	<-uploadControl // esvazia o canal
+
+}
---
+++ UploadS3/go.mod
@@ -0,0 +1,7 @@
+module uploads3
+
+go 1.21.6
+
+require github.com/aws/aws-sdk-go v1.49.24
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
